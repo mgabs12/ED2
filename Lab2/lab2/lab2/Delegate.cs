f9b0f08b79b5fb9edcb3b2b7ed467f828b7f52ab
mgabs12 <98103601+mgabs12@users.noreply.github.com>
2023-09-19 12:47:18 -0600

    Create Delegate.cs

---
+++ Lab2/lab2/lab2/Delegate.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace lab2
+{
+    public class Delegates
+    {
+        public static System.Comparison<Persona> NameComparison = delegate (Persona p1, Persona p2)
+        {
+            return p1.name.CompareTo(p2.name);
+        };
+
+        public static System.Comparison<Persona> DPIComparison = delegate (Persona p1, Persona p2)
+        {
+            return p1.dpi.CompareTo(p2.dpi);
+        };
+    }
+}
