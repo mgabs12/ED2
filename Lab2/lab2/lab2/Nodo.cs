a0934dadf7f9adc71b694f50b8436450b5ce08ab
mgabs12 <98103601+mgabs12@users.noreply.github.com>
2023-09-19 12:47:31 -0600

    Create Nodo.cs

---
+++ Lab2/lab2/lab2/Nodo.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace lab2
+{
+    public class Nodo<T>
+    {
+        public T Value { get; set; }
+        public Nodo<T>? Left { get; set; }
+        public Nodo<T>? Right { get; set; }
+        public int Height { get; set; }
+
+        public Nodo(T value)
+        {
+            this.Value = value;
+            Left = null;
+            Right = null;
+            Height = 1;
+        }
+    }
+}
