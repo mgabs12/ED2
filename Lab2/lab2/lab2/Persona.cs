36b25faca32be094778f59ed8100b1222cbba257
mgabs12 <98103601+mgabs12@users.noreply.github.com>
2023-09-19 12:49:17 -0600

    Create Persona.cs

---
+++ Lab2/lab2/lab2/Persona.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace lab2
+{
+    public class Persona
+    {
+        public string name { get; set; }
+        public string dpi { get; set; }
+        public DateTime datebirth { get; set; }
+        public string address { get; set; }
+    }
+}
