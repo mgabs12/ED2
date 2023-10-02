5741edb15942a8460fd28c77e0c333c2700c390c
mgabs12 <98103601+mgabs12@users.noreply.github.com>
2023-10-01 20:24:08 -0600

    Create Letra.cs

---
+++ Lab2/lab2/lab2/Letra.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace lab2
+{
+    public class Letra
+    {
+        public int frecuencia { get; set; }
+        public double probabilidad { get; set; }
+        public double superior { get; set; }
+        public double inferior { get; set; }
+    }
+}
